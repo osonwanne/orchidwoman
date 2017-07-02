911dddb03af6850dd5eb0e1a645c95e4327b1548
Bon Osonwanne <nosonwan@gmail.com>
2017-07-01 18:04:00 -0700

    product selection and naira settings

--- products/models.py
+++ products/models.py
@@ -4,8 +4,17 @@ from longclaw.longclawproducts.models import ProductVariantBase
 
 class ProductVariant(ProductVariantBase):
 
+    _SIZE_CHOICES = (
+        ('Small', 'Small'),
+        ('Medium', 'Medium'),
+        ('Large', 'Large'),
+        ('X-Large', 'X-Large')
+    )
+
     # Enter your custom product variant fields here
     # e.g. colour, size, stock and so on.
     # Remember, ProductVariantBase provides 'price', 'ref', 'slug' fields
     # and the parental key to the Product model.
     description = RichTextField()
+
+    dress_size = models.CharField(max_length=10, choices=_SIZE_CHOICES, default='Medium')
